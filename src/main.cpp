99c96277cac3393996d9ba4175b15e7df88421bf
Iury Ferreira Santana <criticow@outlook.com>
2025-03-18 11:39:34 -0300

    release build

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,35 @@
+#include <ngyn/ngyn.hpp>
 #include "game.hpp"
 
-int main()
-{
-  ngyn::ngLogger.setFormat("HH:mm:ss");
-  Game game;
-  game.run();
-}
+#ifdef NDEBUG
+  #define WIN32_LEAN_AND_MEAN
+  #undef APIENTRY
+  #include <Windows.h>
+#endif
+
+#ifdef _WIN32
+  #ifdef NDEBUG
+    int WINAPI WinMain(
+      HINSTANCE hInstance,
+      HINSTANCE hPrevInstance,
+      LPSTR lpCmdLine,
+      int nCmdShow
+    )
+    {
+      ngyn::ngLogger.setLevel(ngyn::LoggerLevel::Error);
+      ngyn::ngLogger.setMode(ngyn::LoggerMode::Quiet);
+
+      Game game;
+      game.run();
+      return 0;
+    }
+  #else
+    int main()
+    {
+      ngyn::ngLogger.setFormat("HH:mm:ss");
+      Game game;
+      game.run();
+      return 0;
+    }
+  #endif
+#endif
